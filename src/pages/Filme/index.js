1aa823331ca1c50a656c3b8087e8f877dc0c1c10
Anton <88747866+AntonyGuzma@users.noreply.github.com>
2023-07-10 19:01:28 -0400

    :lipstick: feat: Estilizaçao CSS pagina SOBRE o filme

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,9 +1,52 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import api from '../../services/api'; 
+
+import '../Filme/filme-info.css';
+
 function Filme(){
+
+    const {id} = useParams();
+    const [filme, setFilme] = useState({});
+
+    useEffect(()=>{
+        //requisitando da api o ID pego
+        async function loadFilme(){
+            await api.get(`/movie/${id}`,
+            {
+                params: {
+                    api_key: '28fc232cc001c31e8a031f419d0a14ca', 
+                    language: 'pt-Br'
+                }
+            })
+            .then((response)=>{
+                setFilme(response.data);
+            })
+            .catch(()=>{
+                console.error('Filme Não Encontrado');
+            })
+        }
+
+        loadFilme();
+
+    })
+
     return(
-        <div>
-            <h1>Filme</h1>
+        <div className="filme-info">
+             <h1>{filme.title}</h1>
+             <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title}></img>
+
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+
+            <strong>Avalicação: {filme.vote_average} / 10</strong>
+
+            <div className="area-button">
+                <button>Salvar</button>
+                <button><a href="#">Trailer</a></button>
+            </div>
         </div>
     )
 }
-
+     
 export default Filme;
